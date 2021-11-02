00cf4355850f3c40e7a7cace1bc688c14c024073
Dirk Jäckel <github@dirk.jaeckel.name>
2021-11-02 12:26:33 +0100

    Add start view

---
+++ Multisig/UI/App/StartViewController.swift
@@ -0,0 +1,20 @@
+//
+//  StartViewController.swift
+//  Multisig
+//
+//  Created by Dirk Jäckel on 02.11.21.
+//  Copyright © 2021 Gnosis Ltd. All rights reserved.
+//
+
+import UIKit
+
+class StartViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+}
