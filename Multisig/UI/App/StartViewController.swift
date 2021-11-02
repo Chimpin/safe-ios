bab517bf567118483eff34e45a769ccb6e572e60
Dirk Jäckel <dirk.jaeckel@gnosis.pm>
2021-11-02 16:20:42 +0100

    Connect button with outlet

--- Multisig/UI/App/StartViewController.swift
+++ Multisig/UI/App/StartViewController.swift
@@ -10,11 +10,29 @@ import UIKit
 
 class StartViewController: UIViewController {
 
+    @IBAction func buttonPressed() {
+        print("buttonPressed() | called")
+        let alert = UIAlertController(
+                title: "Hello, world",
+                message: "Slider",
+                preferredStyle: .alert
+        )
+
+        let action = UIAlertAction(
+                title: "Awesome!",
+                style: .default,
+                handler: nil
+        )
+
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+    }
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
-
-
 }
+
