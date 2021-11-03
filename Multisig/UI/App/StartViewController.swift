b30a1e375f447cc97bb419a908ce36126bb29e13
Dmitry Bespalov <dmitry.bespalov@gnosis.pm>
2021-11-03 10:36:01 +0100

    refs/heads/dima-ios-training Created vc

---
+++ Multisig/UI/App/StartViewController.swift
@@ -0,0 +1,18 @@
+//
+//  StartViewController.swift
+//  Multisig
+//
+//  Created by Dmitry Bespalov on 02.11.21.
+//  Copyright © 2021 Gnosis Ltd. All rights reserved.
+//
+
+import UIKit
+
+class StartViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
