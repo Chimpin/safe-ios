78e98dc2f380d8bbf38d3581eca7bf3fab597f40
Dmitry Bespalov <dmitry.bespalov@gnosis.pm>
2021-11-15 11:31:38 +0100

    refs/heads/dima-ios-training

---
+++ Multisig/UI/Assets/MyTableViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  MyTableViewCell.swift
+//  Multisig
+//
+//  Created by Dmitry Bespalov on 15.11.21.
+//  Copyright © 2021 Gnosis Ltd. All rights reserved.
+//
+
+import UIKit
+
+class MyTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+}
