876c370530261ac2fda83f00ed6aa9f56c2bd0ea
Dirk Jäckel <dirk.jaeckel@gnosis.pm>
2021-11-16 11:47:59 +0100

    Use integers for sections

--- Multisig/UI/App/MyTableViewController.swift
+++ Multisig/UI/App/MyTableViewController.swift
@@ -9,29 +9,38 @@
 import UIKit
 
 class MyTableViewController: UITableViewController {
-    
-    var chains: [SCGModels.Chain] = []
-    
+
+    var evenChains: [SCGModels.Chain] = []
+    var oddChains: [SCGModels.Chain] = []
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
         tableView.registerCell(MyTableViewCell.self)
         loadData()
     }
-    
+
     func loadData() {
         let cgw = App.shared.clientGatewayService
-        
+
         cgw.asyncChains { [weak self] result in
             guard let self = self else {
                 return
             }
-            
+
             switch result {
             case .success(let response):
-                
+
                 DispatchQueue.main.async {
-                    self.chains = response.results
+                    self.evenChains = []
+                    self.oddChains = []
+                    response.results.forEach {
+                        if $0.chainId.value % 2 == 0 {
+                            self.evenChains.append($0)
+                        } else {
+                            self.oddChains.append($0)
+                        }
+                    }
                     self.tableView.reloadData()
                 }
 
@@ -44,18 +53,39 @@ class MyTableViewController: UITableViewController {
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 1
+        return 2
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return chains.count
+        if section == 0 {
+            return evenChains.count
+        }
+        return oddChains.count
+
+    }
+
+    override func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let tableHeaderView = TableHeaderView()
+        if (section == 0) {
+            tableHeaderView.set("Even Chains")
+        } else {
+            tableHeaderView.set("Odd Chains")
+        }
+        return tableHeaderView
+
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueCell(MyTableViewCell.self)
-        let chain = chains[indexPath.row]
-        cell.myLabel.text = "Chain #\(chain.chainId): \(chain.chainName)"
+        if indexPath.section == 0 {
+            let chain = evenChains[indexPath.row]
+            cell.myLabel.text = "Chain #\(chain.chainId): \(chain.chainName)"
+        } else {
+            let chain = oddChains[indexPath.row]
+            cell.myLabel.text = "Chain #\(chain.chainId): \(chain.chainName)"
+        }
+
         return cell
     }
 }
